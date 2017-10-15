c8644c9674e4caafe040cba06cc386d36218c095
paperspace <kennyhsu5@gmail.com>
2017-10-15 19:37:11 -0400

    Update classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -8,21 +8,13 @@ import numpy as np
 
 class TLClassifier(object):
     def __init__(self):
-
-	self.det_graph = tf.Graph()
-
-        with self.det_graph.as_default():
-            self.x = tf.placeholder(tf.float32, (None, 200, 300, 3))
-            self.y = tf.placeholder(tf.int32, (None))
-            self.logits = self.LeNet(tf.cast(self.x, tf.float32))
-            self.initialize = tf.global_variables_initializer()
-            self.save = tf.train.Saver()
-            self.sess1= tf.Session()
-            rospack = rospkg.RosPack()
-	    self.save_file = str(rospack.get_path('tl_detector'))+'/light_classification/det_model.ckpt'
-            #self.save_file = str(os.path.join(os.getcwd(), 'trained_variables.ckpt'))
-            self.save.restore(self.sess1, self.save_file)
-	    #rospy.logerr('TF loaded')
+    	rospack = rospkg.RosPack()
+	self.save_file = str(rospack.get_path('tl_detector'))+'/light_classification/model.ckpt'
+	tf.reset_default_graph()
+	self.network = self.ConvNet()
+	self.sess = tf.Session()
+	self.saver = tf.train.Saver()
+	self.saver.restore(self.sess, self.save_file)
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -34,73 +26,50 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-	color = TrafficLight.UNKNOWN
+	label_to_signal = {
+	    0: TrafficLight.RED,
+	    1: TrafficLight.YELLOW,
+	    2: TrafficLight.GREEN,
+            3: TrafficLight.UNKNOWN,
+	}
+	image = image.reshape(1, 64, 96, 3)
+	label = self.sess.run(self.network.inference, feed_dict={
+	    self.network.x: image,
+	    self.network.keep_prob: 1.0
+	})[0]
+	return label_to_signal[label]
 
-	img = self.adjust_gamma(image)
-	img = img.reshape(1,200,300,3)	
+    class ConvNet:
+        def __init__(self):
+            self.x = tf.placeholder(tf.float32, shape=[None, 64, 96, 3], name='x')
+            self.y = tf.placeholder(tf.float32, shape=[None, 4], name='y')
+            self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+        
+            # Layers of NN
+	    self.layer1 = self.conv_layer(self.x, 3, 4, 'layer1')
+            self.layer1_pool = tf.nn.max_pool(self.layer1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+            self.layer2 = self.conv_layer(self.layer1_pool, 3, 4, 'layer2')
+            self.layer2_pool = tf.nn.max_pool(self.layer2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    
+            self.layer3 = self.conv_layer(self.layer2_pool, 3, 8, 'layer3')
+            self.layer3_pool = tf.nn.max_pool(self.layer3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+            self.layer4 = self.conv_layer(self.layer3_pool, 3, 8, 'layer4')
+            self.layer4_pool = tf.nn.max_pool(self.layer4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        
+            flatten_size = 4 * 6 * 8
+            self.flatten = tf.reshape(self.layer4_pool, [-1, flatten_size])
+            self.layer7 = self.fc_layer(self.flatten, 50, 'layer7')
+            self.layer7_dropout = tf.nn.dropout(self.layer7, self.keep_prob)
+            self.logits = tf.contrib.layers.fully_connected(self.layer7_dropout, 4, activation_fn=None, scope='logits') 
+            self.inference = tf.argmax(self.logits, axis=1)
+        
+        def fc_layer(self, x, hh_size, scope):
+            with tf.variable_scope(scope):
+                h1 = tf.contrib.layers.fully_connected(x, hh_size, activation_fn=None, scope='fully_connected')
+                return tf.nn.relu(h1, 'relu')
+        
+        def conv_layer(self, x, kernel_size, filter_size, scope):
+            with tf.variable_scope(scope):
+                h1 = tf.contrib.layers.conv2d(x, filter_size, kernel_size, activation_fn=None, scope='convolution')
+                return tf.nn.relu(h1, 'relu')
 
-        with self.det_graph.as_default():
-            prediction =  self.sess1.run(self.logits, feed_dict={self.x: img})
-
-        value = np.argmax(prediction)	
-	if value == 1:
-            color = TrafficLight.GREEN		
-	elif value == 2:
-            color = TrafficLight.YELLOW		
-	elif value == 3:
-            color = TrafficLight.RED	
-
-        return color
-
-    def adjust_gamma(self, image, gamma=2.0):
-        # build a lookup table mapping the pixel values [0, 255] to
-        # their adjusted gamma values
- 
-        invGamma = 1.0 / gamma
-        table = np.array([((i / 255.0) ** invGamma) * 255
-        for i in np.arange(0, 256)]).astype("uint8")
-        return cv2.LUT(image, table)
-
-
-    def conv2D(self,in_layer, shape1, zeros1, mu = 0,sigma = 0.1):
-        # Layer: Convolutional
-        _conv1_W = tf.Variable(tf.truncated_normal(shape=shape1, mean = mu, stddev = sigma))
-        _conv1_b = tf.Variable(tf.zeros(zeros1))
-        _conv1   = tf.nn.conv2d(in_layer, _conv1_W, strides=[1, 1, 1, 1],padding='VALID') + _conv1_b
-
-        # Activation.
-        _conv1 = tf.nn.relu(_conv1)
-
-        # Pooling
-        _conv1 = tf.nn.max_pool(_conv1, ksize=[1, 2, 2, 1], strides=[1, 2,2, 1], padding='VALID')
-        return _conv1
-
-
-    def deconv2D(self,in_layer, shape1, zeros1, mu = 0,sigma = 0.1, activate=True):
-
-        # Fully connected
-        _fc1_W = tf.Variable(tf.truncated_normal(shape=shape1, mean = mu,stddev = sigma))
-        _fc1_b = tf.Variable(tf.zeros(zeros1))
-        _fc1   = tf.matmul(in_layer, _fc1_W) + _fc1_b
-        if activate == True:
-            _fc1    = tf.nn.relu(_fc1)
-
-        return _fc1
-
-
-
-    def LeNet(self,img):
- 
-        layer1 = self.conv2D(img,(5, 5, 3, 16), 16)
-        layer2 = self.conv2D(layer1,(5, 5, 16, 32), 32)
-        layer3 = self.conv2D(layer2,(3, 3, 32, 64), 64)
-
-        fc0   = flatten(layer3)
-
-        outlayer1 =  self.deconv2D(fc0,(49280, 150), 150)
-        outlayer2 =  self.deconv2D(outlayer1,(150, 100), 100)
-        outlayer3 =  self.deconv2D(outlayer2,(100, 50), 50)
-        outlayer4 =  self.deconv2D(outlayer3,(50, 4), 4, activate=False)
-
-        return outlayer4
-	
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -52,7 +52,6 @@ class TLDetector(object):
         self.config = yaml.load(config_string)
 	
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)       
-        
         rospy.spin()
 
     def pose_cb(self, msg):
@@ -121,88 +120,6 @@ class TLDetector(object):
         y_diff = (p1y-p2y)*(p1y-p2y)     
         sqrt1 = math.sqrt(x_diff+y_diff) 
         return sqrt1
-	
-
-    def project_to_image_plane(self, point_in_world1, point_in_world2):
-        """Project point from 3D world coordinates to 2D camera image location
-
-        Args:
-            point_in_world (Point): 3D location of a point in the world
-
-        Returns:
-            x (int): x coordinate of target point in image
-            y (int): y coordinate of target point in image
-
-        """
-
-        fx = self.config['camera_info']['focal_length_x']
-        fy = self.config['camera_info']['focal_length_y']
-        image_width = self.config['camera_info']['image_width']
-        image_height = self.config['camera_info']['image_height']
-
-        # get transform between pose of camera and world frame
-        trans = None
-        try:
-            now = rospy.Time.now()
-            self.listener.waitForTransform("/base_link",
-                  "/world", now, rospy.Duration(1.0))
-            (trans, rot) = self.listener.lookupTransform("/base_link",
-                  "/world", now)
-
-        except (tf.Exception, tf.LookupException, tf.ConnectivityException):
-            rospy.logerr("Failed to find camera to map transform")
-
-        #Use tranform and rotation to calculate 2D position of light in image	
-        #Used as reference point for camera calibration
-	c_x = point_in_world1
-        c_y = point_in_world2
-
-        #c_x = point_in_world[0]
-        #c_y = point_in_world[1]
-
-        #rospy.logerr ("point x: " + str(c_x) + "point y: "+ str(c_y))
-        
-        #Transformation of coordinate system to a car coordinate system
-        #http://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html
-        #http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToEuler/
-		
-        x = self.pose.pose.orientation.x
-        y = self.pose.pose.orientation.y
-        z = self.pose.pose.orientation.z
-        w = self.pose.pose.orientation.w
-	
-	#rospy.logerr ("Pose x: " + str(x))	
-
-        #heading = atan2(2*qy*qw-2*qx*qz , 1 - 2*qy2 - 2*qz2)
-        val1 = 2.0*y*w-2.0*x*z
-        val2 = 1.0 - 2.0*y*y - 2.0*z*z	
-        heading = math.degrees(math.atan2(val1, val2))
-
-        #rospy.logerr ("Heading: " + str(heading))	
-        
-        #define coordinates (origin)
-
-	x_1 = (c_y-self.pose.pose.position.y)*math.sin(math.radians(heading))-(self.pose.pose.position.x- c_x)*math.cos(math.radians(heading))
-	y_1 = (c_y-self.pose.pose.position.y)*math.cos(math.radians(heading))-(c_x-self.pose.pose.position.x)*math.sin(math.radians(heading))
-
-
-	#x_1 = 0
-        #y_1 = 0
-        
-        objectPoints = np.array([[float(x_1), float(y_1), 0]],dtype = np.float32)
-        rvec = (0,0,0) 	
-        tvec = (0,0,0) 	
-        cameraMatrix = np.array([[fx,0,image_width/2],
-                                [0,fy,image_height/2],  	
-                                 [0,0,1]])
-        distCoeffs = None
-        ret,_ = cv2.projectPoints(objectPoints, rvec,tvec,cameraMatrix, distCoeffs)	
-
-        x2 = int(ret[0,0,0])
-        y2 = int(ret[0,0,1])
-
-	#rospy.logerr("x: "+str(x2))
-        return (x2, y2)
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -218,11 +135,10 @@ class TLDetector(object):
             self.prev_light_loc = None
             return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-	img = cv2.resize(cv_image, (300,200))
-        tmp = self.light_classifier.get_classification(img)
-	#imsave(str(tmp) + "-" + str(time.time()) + ".png", img)
-	return tmp
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "rgb8")
+	img = cv2.resize(cv_image, (96,64))
+        prediction = self.light_classifier.get_classification(img)
+	return prediction
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
@@ -242,24 +158,24 @@ class TLDetector(object):
         if(self.pose):
             car_position = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
 
-	min_pos_diff = 99999
-	state, tmp = 4, 0
-	for pos in stop_line_positions:
-	    # Better to compute distanc to stop line based on coordinates, but we are given position so
-	    # use that for now.
-	    light_wp_position = self.get_closest_waypoint(pos[0], pos[1])
-	    diff = light_wp_position - car_position
-	    # Only use stop lines that are ahead in waypoint position of the car.
-	    # Edge case when the track wraps around need to be handled later
-	    if diff > 0 and diff < min_pos_diff:
-		min_pos_diff = diff
-		light = light_wp_position
-		state = self.lights[tmp].state
-	    tmp += 1
+	    min_pos_diff = 99999
+	    state, tmp = 4, 0
+	    for pos in stop_line_positions:
+	        # Better to compute distanc to stop line based on coordinates, but we are given position so
+	        # use that for now.
+	        light_wp_position = self.get_closest_waypoint(pos[0], pos[1])
+	        diff = light_wp_position - car_position
+	        # Only use stop lines that are ahead in waypoint position of the car.
+	        # Edge case when the track wraps around need to be handled later
+	        if diff > 0 and diff < min_pos_diff:
+		    min_pos_diff = diff
+		    light = light_wp_position
+		    state = self.lights[tmp].state
+	        tmp += 1
 
-        if light:
-            #state = self.get_light_state(light)
-            return light, state
+        if light and min_pos_diff < 150:
+            pred_state = self.get_light_state(light)
+            return light, pred_state
         return -1, TrafficLight.UNKNOWN
 
 if __name__ == '__main__':
